80dde1c9fcb4840b8a681ab087d5de402b4082e9
renatoTheoJr <renatotheojunior@gmail.com>
2022-08-31 15:50:44 -0300

    Adicionando tsyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -11,8 +13,13 @@ interface IRequest {
  * Acessar o repositório
  * Retornar algo
  */
+
+@injectable()
 class CreateCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {}
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: ICategoriesRepository
+    ) {}
 
     async execute({ name, description }: IRequest): Promise<void> {
         const categoryAlreadyExists =
