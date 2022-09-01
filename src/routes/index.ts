4086988876533a89f3afdfcae8371e1d008b0ccb
renatoTheoJr <renatotheojunior@gmail.com>
2022-08-31 23:03:00 -0300

    Authenticate

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.route";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specification.route";
 import { usersRoutes } from "./users.routes";
@@ -9,4 +10,5 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 export { router };
