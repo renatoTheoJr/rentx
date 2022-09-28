63a8f9309fe7d0ee6e8eddb29abc2dae5aee16b0
renatoTheoJr <renatotheojunior@gmail.com>
2022-09-28 20:14:39 -0300

    Devolution rentals

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -17,6 +17,12 @@ class DayjsDateProvider implements IDateProvider {
         const end_date_utc = this.convertToUtc(end_date);
         return dayjs(end_date_utc).diff(start_date_utc, "hours");
     }
+
+    compareInDays(start_date: Date, end_date: Date): number {
+        const start_date_utc = this.convertToUtc(start_date);
+        const end_date_utc = this.convertToUtc(end_date);
+        return dayjs(end_date_utc).diff(start_date_utc, "days");
+    }
 }
 
 export { DayjsDateProvider };
--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticate";
 
@@ -9,3 +10,10 @@ const createRentalController = new CreateRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 export { rentalRoutes };
+
+const devolutionRentalController = new DevolutionRentalController();
+rentalRoutes.post(
+    "/devolution/:id",
+    ensureAuthenticated,
+    devolutionRentalController.handle
+);
